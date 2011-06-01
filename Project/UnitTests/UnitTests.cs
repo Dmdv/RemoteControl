11a18ebf36bc0ba776b48edcd52d2ea492ccb3e3
Dm Dv <dimos-d@yandex.ru>
2011-06-01 16:15:13 +0400

    Added UnitTests

---
+++ Project/UnitTests/UnitTests.cs
@@ -0,0 +1,41 @@
+﻿using GTalk.Client;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    /// <summary>
+    /// UnitTests.
+    /// </summary>
+    [TestClass]
+    public class UnitTests
+    {
+        [ClassInitialize]
+        public static void MyClassInitialize(TestContext testContext)
+        {
+        }
+
+        [ClassCleanup]
+        public static void MyClassCleanup()
+        {
+        }
+
+        [TestInitialize]
+        public void MyTestInitialize()
+        {
+        }
+
+        [TestCleanup]
+        public void MyTestCleanup()
+        {
+        }
+
+        [TestMethod]
+        public void TestPeerLogin()
+        {
+            //var peer = new Peer("141700.info@gmail.com");
+            //peer.Password = "****";
+            //peer.Login();
+            //peer.Logout();
+        }
+    }
+}
